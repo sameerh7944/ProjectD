d3b0bac65d8c9d8ced502b25b55b6531b7eab8eb
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-30 21:20:00 -0500

    new variable

--- src/Demo1D/Demo1DTest.java
+++ src/Demo1D/Demo1DTest.java
@@ -4,6 +4,9 @@ public class Demo1DTest {
 public static void main(String[]args) {
 		
 		System.out.println("United States Of America");
+		  char myChar = 'X'; 
+
+	        System.out.println("The value of myChar is: " + myChar);
 }
 
 }
